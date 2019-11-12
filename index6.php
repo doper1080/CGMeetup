c500ca0851db947bf66d05073cc1562ec334c0a1
doper1080 <55241362+doper1080@users.noreply.github.com>
2019-11-13 03:27:16 +0700

    Add files via upload

---
+++ index6.php
@@ -0,0 +1,89 @@
+<!DOCTYPE php>
+<php lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CGMeetupVideo</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container pb-3">
+        <div class="d-flex justify-content-center">
+            <h1>CGMeetup <h1 class="text-danger">VIDEO</h1></h1></a>
+        </div>
+    </div>
+    <div class="container">
+            <div class="d-flex justify-content-center">
+                    <div class="row">
+                            <div class="col">
+                                <video width="550" controls>
+                                    <source src="video/6.mp4" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                        <div class="row">
+                            <div class="col">
+                                <h5>'Watermelon A Cautionary Tale' by Kefei Li & Connie Qin He</h5>
+                            </div>
+                        </div>
+                            </div>
+                            <div class="col">
+                                <div class="d-flex justify-content-center">
+                                    <table class="table table-hoover">
+                                        <tr>
+                                            <td>
+                                                <a href="index2.php"><img src="image/thmb2.png" width="120" class="mr-3" alt="..."></a>
+                                            </td>
+                                            <td>
+                                                <a href="index2.php"><h6>A Lovely Mess' by Lucien Godin, Hugo Durand</h5></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="index3.php"><img src="image/thmb3.png" width="120" class="mr-3" alt="..."></a>
+                                            </td>
+                                            <td>
+                                                <a href="index3.php"><h6>'Dead Friends' by Changsik Lee</h5></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="index4.php"><img src="image/thmb4.png" width="120" class="mr-3" alt="..."></a>
+                                            </td>
+                                            <td>
+                                                <a href="index4.php"><h6>'Mind Games' by Jiaqi Emily Yan</h5></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="index5.php"><img src="image/thmb5.png" width="120" class="mr-3" alt="..."></a>
+                                            </td>
+                                            <td>
+                                                <a href="index5.php"><h6>'Sticks & Stones' by Anna Conde, Mark Lim, Andrea Walker</h5></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a href="index.php"><img src="image/thmb1.png" width="120" class="mr-3" alt="..."></a>
+                                            </td>
+                                            <td>
+                                                <a href="index.php"><h6>'CICLO Animated Short Film' by Felipe Del Rio</h5></a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>         
+                            </div>
+                        </div>
+            </div>
+    </div>
+    
+    
+</body>
+</php>
